d696a72be0af1aa44b0033483034f3a0df6becb6
NereaRubio <nerea.rubio.almeida@gmail.com>
2023-02-27 10:13:15 +0100

    update2

--- 15_busco5_odb10.sh
+++ 15_busco5_odb10.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#SBATCH --job-name=15.5_busco5_odb10
+#SBATCH --job-name=15_busco5_odb10
 #SBATCH --error %x-%j.err
 #SBATCH --output %x-%j.out
 
@@ -10,9 +10,9 @@
 
 module load busco/4.1.4
 
-IN=/cluster/home/s_lchueca/engraulis_encrasicolus_genome/00.2_rna_ena/14_trinity/ena_5_transcriptome.fa
+IN=/cluster/home/s_lchueca/engraulis_encrasicolus_genome/00.2_rna_ena/14_trinity/ena_enen_transcriptome.fa
 LIN=/cluster/software/busco/datasets/odb10_2021_09_02/actinopterygii_odb10
-OUT=Ena_5_dowload_busco5_odb10
+OUT=Ena_enen_dowload_busco5_odb10
 M=transcriptome
 
 busco -i ${IN} -l ${LIN} -c 8 -o ${OUT} -m ${M} --long --offline
