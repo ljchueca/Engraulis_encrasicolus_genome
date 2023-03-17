39ee673e0ade162dfa68e6eca7a69ec7bd7314b1
NereaRubio <nerea.rubio.almeida@gmail.com>
2023-03-17 13:38:07 +0100

    update

---
+++ 30_busco_summaries.sh
@@ -0,0 +1,6 @@
+#En local con phyton3/3.7.4
+
+#Working directory
+WD=/cluster/home/s_lchueca/engraulis_encrasicolus_genome/busco_summaries
+
+/cluster/software/busco/busco-3.1.0/scripts/generate_plot.py -wd ${WD} 
