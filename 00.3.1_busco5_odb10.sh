253412e9ab7bf175b47e133c12bd7afac1628cfc
NereaRubio <nerea.rubio.almeida@gmail.com>
2023-03-21 13:25:10 +0100

    update

--- 00.3.1_busco5_odb10.sh
+++ 00.3.1_busco5_odb10.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#SBATCH --job-name=00.1.1.1_busco5_odb10
+#SBATCH --job-name=00.3.1._busco5_odb10
 #SBATCH --error %x-%j.err
 #SBATCH --output %x-%j.out
 
@@ -14,11 +14,11 @@ module load busco/5.2.2_ve
 #Loading requirement: perl/5.34.0 gmap/2021-12-17 blat/385 fasta/36.3.8h_04-May-2020 mariadb/10.4.10 samtools/1.15.1 pasa/2.4.1 augustus/3.4.0
 
 #Input directory
-IN=/cluster/home/lchueca/TBG_3759_Enen_assembly/00.1_hifiasm/Enen_hifiasm.bp.p_ctg.fa
+IN=
 #Linage
 LIN=/cluster/software/busco/datasets/odb10_2021_09_02/actinopterygii_odb10
 #Output directory
-OUT=00.1_busco5_odb10
+OUT=00.3_busco5_odb10
 #CPU per task
 CPU=8
 #Mode
