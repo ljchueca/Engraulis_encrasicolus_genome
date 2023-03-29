444e1af8b6006ed8cca091fdbdad645d2f94530c
NereaRubio <nerea.rubio.almeida@gmail.com>
2023-03-29 13:37:32 +0200

    Corregidos

--- 00.1.1.3_blobtools.sh
+++ 00.1.1.3_blobtools.sh
@@ -8,7 +8,7 @@
 #SBATCH --mem=236G
 #SBATCH --cpus-per-task=96
 
-module load blobtools/1.1.1 ncbi-blast/2.12.0 
+module load blobtools/1.1.1 ncbi-blast/2.12.0 samtools/1.15.1 
 
 #Input assembly
 AS=Enen_hifiasm.bp.p_ctg.fa
@@ -21,16 +21,19 @@ T=00.1_hifiasm.nt.hsp1
 #Output prefix
 OUT=Enen_blobplot
 
-
+#0. index bam file 
+samtools index ${BA} ${BA}.bai
 
 #1.Reduce the size
 blobtools map2cov -i ${AS} -b ${BA} &&
 
 #2. make the hits file by using blastn
-blastn -task megablast -query ${AS} -db ${DT} -outfmt '6 qseqid staxids bitscore std' -max_target_seqs 1 -max_hsps 1 -num_threads 96 -evalue 1e-25 -out ${T}.out >${T}.log 2>${T}.err &&
+blastn -task megablast -query ${AS} -db ${DT} \
+ -outfmt '6 qseqid staxids bitscore std' -max_target_seqs 1 -max_hsps 1 -num_threads 96 \
+ -evalue 1e-25 -out ${T}.out >${T}.log 2>${T}.err &&
 
 #3. Create a BlobDB
-blobtools create -i ${AS} -b ${BA}.cov -t ${T}.out -o ${OUT} >${OUT}.log 2>${OUT}.err  && \
+blobtools create -i ${AS} -c ${BA}.cov -t ${T}.out -o ${OUT} >${OUT}.log 2>${OUT}.err  && \
 
 #4. Generate a tabular view
 blobtools view -i ${OUT}.blobDB.json && \
--- 00.4_mitohifi.sh
+++ 00.4_mitohifi.sh
@@ -11,15 +11,15 @@
 module load mitohifi/2.2 
 #Previamente se han convertido los reads del formato fastq a fasta mediante seqtk seq -a m64037e_230114_104235.deepconsensus.fastq > m64037e_230114_104235.deepconsensus.fasta
 #Reads in fasta
-RE=m64037e_230114_104235.deepconsensus.fasta
+RE=m64037e_230114_104235.deepconsensus.fa
 #Close-related Mitogenome is fasta 
 F=mitochondrial_sequence.fa
-#Close-related Mitogenome is genbak format
-GBK=mitochondrial_sequence.gbk
+#Close-related Mitogenome is genbank format
+GBK=sequence.gb
 #CPU per task
 CPU=96
 
-MitoHiFi -r ${RE} -f ${F} -g ${GBK} -t ${CPU} -a {animal}
+mitohifi.py -r ${RE} -f ${F} -g ${GBK} -t ${CPU} -a animal
 
 
 
